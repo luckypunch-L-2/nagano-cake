5d905bc232ce4506edd4ab613332307150abf11b
EC2 Default User <ec2-user@ip-172-31-6-10.ap-northeast-1.compute.internal>
2023-09-24 09:13:23 +0000

    route~

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,18 @@
 Rails.application.routes.draw do
+
+  get 'admin' => 'admin/homes#top'
+
+  root to: "public/homes#top"
+  get 'about' => 'public/homes#about'
+
+  get 'items' => 'public/items#index'
+  get 'items/:id' => 'public/items#show'
+
   scope module: :public do
     post 'orders/confirm' => 'orders#confirm'
     get 'orders/complete' => 'orders#complete'
     resources :orders, only: [:new, :create, :index, :show]
   end
-
-  root to: "public/homes#top"
-  get 'about' => 'public/homes#about'
 
   scope module: :public do
     get "customers/my_page" => "customers#show"
@@ -19,29 +25,24 @@ Rails.application.routes.draw do
     patch '/customers/withdrawal' => 'customers#withdrawal'
   end
 
+  scope module: :public do
+    resources :cart_items, only: [:index, :create, :update, :destroy]
+  end
+
   #顧客
   # URL /customers/sign_in ...
   devise_for :customers, controllers: {
   registrations: "public/registrations",
   sessions: 'public/sessions'
-}
+  }
 
-
-
-  get 'items' => 'public/items#index'
-  get 'items/:id' => 'public/items#show'
-  
-  scope module: :public do
-    resources :cart_items, only: [:index, :create, :update, :destroy]
-  end
-  
   delete 'cart_items/destroy_all' => 'public/cart_items#desstroy_all'
 
   # 管理者
   # URL /admin/sign_in ...
   devise_for :admin, skip: [:registrations, :passwords] ,controllers: {
   sessions: "admin/sessions"
-}
+  }
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
