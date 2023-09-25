2cf9e601a78c4e5525c1b587e43d011f959441e7
SeiyaYamaguchi <141209682+seiya0331@users.noreply.github.com>
2023-09-25 07:27:49 +0000

    admin/customers追加

--- app/controllers/admin/customers_controller.rb
+++ app/controllers/admin/customers_controller.rb
@@ -1,2 +1,26 @@
 class Admin::CustomersController < ApplicationController
+    def index
+        @customers = Customer.all.page(params[:page])
+    end
+
+    def show
+        @customer = Customer.find(params[:id])
+        @customers = Customer.all.page(params[:page])
+    end
+
+    def edit
+        @customer = Customer.find(params[:id])
+    end
+
+    def update
+        @customer = Customer.find(params[:id])
+        @customer.update(customer_params)
+        flash[:notice] = "会員情報を編集しました"
+        redirect_to admin_customer_path
+    end
+
+    private
+    def customer_params
+        params.require(:customer).permit(:last_name, :first_name, :last_name_kana, :first_name_kana, :zip_code, :address, :telephone_number, :email, :is_deleted)
+    end
 end
