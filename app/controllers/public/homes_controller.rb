3b5fd65c9d88aca6cc771b4902584b7ecb831c30
EC2 Default User <ec2-user@ip-172-31-33-245.ap-northeast-1.compute.internal>
2023-09-27 13:04:43 +0000

    トップ画面

--- app/controllers/public/homes_controller.rb
+++ app/controllers/public/homes_controller.rb
@@ -1,6 +1,8 @@
 class Public::HomesController < ApplicationController
   def top
     @items = Item.order('id DESC').limit(4)
+    @genres = Genre.all
+    @genred_items = Item.where(genre_id: params[:genre_id])
   end
 
   def about
