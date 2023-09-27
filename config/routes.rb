26a17169e38ff01b1113fa45f5d2df011f500ce4
EC2 Default User <ec2-user@ip-172-31-6-10.ap-northeast-1.compute.internal>
2023-09-27 03:02:16 +0000

    レイアウトの調整

--- config/routes.rb
+++ config/routes.rb
@@ -1,28 +1,14 @@
 Rails.application.routes.draw do
 
-  namespace :admin do
-    resources :orders, only: [:show]
-  end
-
-  scope module: :public do
-    post 'orders/confirm' => 'orders#confirm'
-    get 'orders/complete' => 'orders#complete'
-    resources :orders, only: [:new, :create, :index, :show]
-  end
-
-
-  get 'admin' => 'admin/homes#top'
-
+    #顧客
+  # URL /customers/sign_in ...
+  devise_for :customers, controllers: {
+  registrations: "public/registrations",
+  sessions: 'public/sessions'
+  }
 
   root to: "public/homes#top"
   get 'about' => 'public/homes#about'
-
-
-  scope module: :public do
-    post 'orders/confirm' => 'orders#confirm'
-    get 'orders/complete' => 'orders#complete'
-    resources :orders, only: [:new, :create, :index, :show]
-  end
 
   scope module: :public do
     get "customers/my_page" => "customers#show"
@@ -35,27 +21,20 @@ Rails.application.routes.draw do
     patch '/customers/withdrawal' => 'customers#withdrawal'
   end
 
-
-  namespace :admin do
-    resources :customers, only: [:index, :show, :edit, :update]
-  end
-
-  #顧客
-  # URL /customers/sign_in ...
-  devise_for :customers, controllers: {
-  registrations: "public/registrations",
-  sessions: 'public/sessions'
-}
-
-
   scope module: :public do
     resources :items, only: [:index, :show]
   end
 
- delete 'cart_items/destroy_all' => 'public/cart_items#destroy_all'
-
   scope module: :public do
     resources :cart_items, only: [:index, :create, :update, :destroy]
+  end
+
+  delete 'cart_items/destroy_all' => 'public/cart_items#destroy_all'
+
+  scope module: :public do
+    post 'orders/confirm' => 'orders#confirm'
+    get 'orders/complete' => 'orders#complete'
+    resources :orders, only: [:new, :create, :index, :show]
   end
 
 
@@ -65,10 +44,19 @@ Rails.application.routes.draw do
   sessions: "admin/sessions"
 }
 
+    get 'admin' => 'admin/homes#top'
+
   namespace :admin do
     resources :items, only: [:index, :new, :create, :show, :edit, :update]
     resources :genres, only: [:index, :create, :edit, :update]
+  end
 
+  namespace :admin do
+    resources :customers, only: [:index, :show, :edit, :update]
+  end
+
+    namespace :admin do
+    resources :orders, only: [:show]
   end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
